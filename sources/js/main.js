a89f3dfe23a1b6cfb8f47350f845d638985df306
punitharaggu <punithamonisha@gmail.com>
2019-04-19 16:37:51 +0200

    included Javascript file created eventlistener func and buttons

---
+++ sources/js/main.js
@@ -0,0 +1,9 @@
+
+//func for clicking on Add button
+document.getElementById('add').addEventListener('click', function() {
+var value = document.getElementById('task').value;
+if (value) {
+//  console.log("entry");
+    
+  }
+});
